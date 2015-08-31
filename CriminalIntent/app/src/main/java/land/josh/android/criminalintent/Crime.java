8db72be7214d04984ccaf095c74458a512d9986b
Joshua Johnson <thingster@gmail.com>
2015-08-30 17:20:03 -0700

    WIP: Fragments

---
+++ CriminalIntent/app/src/main/java/land/josh/android/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package land.josh.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by josh on 8/30/15.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+}
